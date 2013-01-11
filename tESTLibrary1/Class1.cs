3e16a3c9b9cf30b0152e60d82d708e1ae8fe02a3
sreeharipv <sreeharipv@gmail.com>
2013-01-12 00:43:59 +0530

    test

--- tESTLibrary1/Class1.cs
+++ tESTLibrary1/Class1.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
-namespace tESTLibrary1
+namespace tESTLibrary1 
+
 {
     public class Class1
     {
-        [Fact]
+        [Fact(Skip="to test")]
         public void testc()
         {
             Assert.True(1 == 1);
